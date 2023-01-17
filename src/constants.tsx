7e1a5906c0e55ed49259d14d162b44658101afcb
AlonBru <AlonBru@users.noreply.github.com>
2023-01-17 12:14:43 +0000

    deploy: 00b2984d3d8b76f0d97c78e7ebe0a8da8f762de7

--- src/constants.tsx
+++
@@ -1,2 +0,0 @@
-export const PLAYER_MOVEMENT = 1;
-export const BOARD_SIZE = 8;
--- src/helpers/board.ts
+++
@@ -1,7 +0,0 @@
-
-export function getFreshBoard ( size:number ):Board {
-
-  return new Array( size ).fill( null )
-  .map( () => new Array( size ).fill( null ) );
-
-}
